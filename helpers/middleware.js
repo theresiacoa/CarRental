81a01a230525af3d0cd6c199616c92d5de7fb7d9
Theresia Coanata <theresiacoa@DESKTOP-K5BVEFS.localdomain>
2019-01-31 15:37:44 +0700

    add middleware

--- helpers/middleware.js
+++ helpers/middleware.js
@@ -3,14 +3,16 @@ function middleware(status = null) {
   return (req, res, next) => {
     if (!req.session.userLoggedIn) {
       res.redirect('/user/login');
-    } else if (req.session.userLoggedIn.status === status) {
-      next()
     } else {
-      res.redirect('/user/login');
+      if (req.session.userLoggedIn) {
+        next();
+      } else if (status && req.session.userLoggedIn.status === status) {
+        next();
+      } else {
+        res.redirect('/user/login');
+      }
     }
   }
-
-
 }
 
 module.exports = middleware;
